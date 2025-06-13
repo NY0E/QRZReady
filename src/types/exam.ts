e7fc5ba71f1cc340e5751572c873ed38a435d5a7
NY0E <steve@maydaymail.com>
2025-06-13 14:26:31 -0500

    Update exam.ts

--- src/types/exam.ts
+++ src/types/exam.ts
@@ -1,1 +1,28 @@
+export type ExamType = 'technician' | 'general' | 'extra';
 
+export interface Question {
+  id: string;
+  question: string;
+  answers: string[];
+  correct: number;
+  refs: string;
+  subelement: string;
+  section: string;
+}
+
+export interface UserProgress {
+  [questionId: string]: {
+    correct: number;
+    incorrect: number;
+    consecutiveCorrect: number;
+    lastSeen: number;
+  };
+}
+
+export interface TestScore {
+  examType: ExamType;
+  score: number;
+  totalQuestions: number;
+  date: number;
+  timeSpent: number;
+}
